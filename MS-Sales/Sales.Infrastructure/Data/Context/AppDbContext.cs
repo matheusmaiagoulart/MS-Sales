5417aa47cdd94a047387262328c337a99980d5d0
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-23 10:30:42 -0300

    feat: create AppDbContext

---
+++ MS-Sales/Sales.Infrastructure/Data/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Domain.Models;
+
+namespace Sales.Infrastructure.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
