d8734c731cd358a2a7cfa12aa40147adb62ea38f
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-09-23 10:31:29 -0300

    feat: mapping table Order

---
+++ MS-Sales/Sales.Infrastructure/Data/MappingTables/OrderMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sales.Domain.Models;
+
+namespace Sales.Infrastructure.Data.MappingTables
+{
+    public class OrderMap : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> e)
+        {
+            e.HasKey(o => o.IdSale);
+            e.OwnsMany(o => o.OrdemItens, p =>
+            {
+                p.Property(pi => pi.IdProduct).IsRequired();
+                p.Property(pi => pi.Quantity).IsRequired();
+            });
+            e.Property(o => o.TotalAmount).IsRequired().HasColumnType("decimal(18,2)");
+            e.Property(o => o.Status).IsRequired();
+            e.Property(o => o.CreatedAt).IsRequired();
+            e.Property(o => o.UpdatedAt).IsRequired();
+            e.ToTable("Orders");
+        }
+    }
+}
